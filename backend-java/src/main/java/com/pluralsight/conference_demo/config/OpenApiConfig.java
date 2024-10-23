96b73e78f1e499552d686afec95a3cc761040af9
jecaro094 <jecarobd@gmail.com>
2024-10-23 08:31:28 +0200

    Added `Swagger` documentation.

---
+++ backend-java/src/main/java/com/pluralsight/conference_demo/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.pluralsight.conference_demo.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("My API")
+                        .version("1.0")
+                        .description("This is a sample Spring Boot 3 API using OpenAPI documentation."));
+    }
+}
